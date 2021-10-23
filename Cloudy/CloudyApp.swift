39ad92d49d617476d516ed41cd99b70b86f8549c
Alex M <nyfa117@gmail.com>
2021-10-22 19:05:45 -0700

    Initial Commit

---
+++ Cloudy/CloudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudyApp.swift
+//  Cloudy
+//
+//  Created by Alex Mokretsov on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CloudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cloudy
+//
+//  Created by Alex Mokretsov on 10/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
